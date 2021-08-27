31c8bec4da7d818cce9365ffb0d8c50819f6d681
Dem1se <dem1se.io@protonmail.com>
2021-08-28 02:55:40 +0530

    Add image to AboutConvener section

--- components/AboutConvener.jsx
+++ components/AboutConvener.jsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import Image from 'next/image'
 import styles from '../styles/AboutConvener.module.css'
 
 export default class AboutConvener extends React.Component {
@@ -7,6 +8,17 @@ export default class AboutConvener extends React.Component {
       <div className={styles.container} id="AboutConvener">
         <h4 className={styles.heading}>CONVENER</h4>
         <hr className={styles.titleUnderline} />
+        <div className={styles.imageBox}>
+          <Image
+            layout='intrinsic'
+            objectFit='cover'
+            src="/ConvenerSectionImage.jpeg"
+            alt="Picture of Principal, Convener and Chief Organizing Committee"
+            width="470"
+            height="320"
+          />
+        </div>
+        <p className={styles.imageCaption}>The Principal presents the Convener with a copy of the <a href="/IVCMR_21_BOOK_OF_ABSTRACTS.pdf">Book of Abstracts</a>, accompanied by the Chief Organizing Committee.</p>
         <table className={styles.table}>
           <tbody>
             <tr>
